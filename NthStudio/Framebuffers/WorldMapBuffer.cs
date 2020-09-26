22672c2a8c81dae767c711cb3bf0d229e55d8caf
Stylianos Polychroniadis <syscon_tech@yahoo.gr>
2020-09-27 01:38:37 +0300

    Map buffers

---
+++ NthStudio/Framebuffers/WorldMapBuffer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NthStudio.Framebuffers
+{
+    using NthDimension.Rendering.Drawables.Framebuffers;
+    public class WorldMapBuffer
+    {
+        Framebuffer compositeMap;
+        Framebuffer vectorCountriesMap;
+        Framebuffer satelitesMap;
+        Framebuffer earthquakesMap;
+        Framebuffer textMap;
+    }
+}
